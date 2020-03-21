cfd7f8b9271ade28507bd63799825eefad4b6ccf
Iman <imanghafoori1@gmail.com>
2020-03-21 01:57:08 +0000

    Apply fixes from StyleCI

--- src/VideoStreamer.php
+++ src/VideoStreamer.php
@@ -77,12 +77,12 @@ class VideoStreamer
         $length = $this->end - $this->start + 1;
         fseek($this->stream, $this->start);
         header('HTTP/1.1 206 Partial Content');
-        header('Content-Length: ' . $length);
-        header("Content-Range: bytes $this->start-$this->end/" . $this->size);
+        header('Content-Length: '.$length);
+        header("Content-Range: bytes $this->start-$this->end/".$this->size);
     }
 
     /**
-     * close curretly opened stream
+     * close curretly opened stream.
      */
     private function end()
     {
@@ -97,7 +97,7 @@ class VideoStreamer
     {
         $i = $this->start;
         set_time_limit(0);
-        while (!feof($this->stream) && $i <= $this->end) {
+        while (! feof($this->stream) && $i <= $this->end) {
             $bytesToRead = $this->buffer;
             if (($i + $bytesToRead) > $this->end) {
                 $bytesToRead = $this->end - $i + 1;
