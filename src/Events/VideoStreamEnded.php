4bd8b48315da931a6e547911973da4ac0352bba8
Iman <imanghafoori1@gmail.com>
2020-07-08 07:57:49 +0000

    Apply fixes from StyleCI

--- src/Events/VideoStreamEnded.php
+++ src/Events/VideoStreamEnded.php
@@ -2,11 +2,8 @@
 
 namespace Iman\Streamer\Events;
 
-use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 use Iman\Streamer\Video;
@@ -36,7 +33,6 @@ class VideoStreamEnded
     {
         return $this->video;
     }
-
 
     /**
      * Get the channels the event should broadcast on.
